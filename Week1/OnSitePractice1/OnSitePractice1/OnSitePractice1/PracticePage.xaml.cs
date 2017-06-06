da53eb2b261665fe253e8e957cb551cbda1b024b
Luis Pujols <luispujols@hotmail.com>
2017-06-06 01:54:05 -0400

    ADD 'Created and completed the OnsitePractice1'

---
+++ Week1/OnSitePractice1/OnSitePractice1/OnSitePractice1/PracticePage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace OnSitePractice1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PracticePage : ContentPage
+    {
+        public PracticePage()
+        {
+            InitializeComponent();
+            var mainStackLayout = new StackLayout();
+            mainStackLayout.Padding = 16;
+
+            var labelTitle = new Label
+            {
+                Text = "Enter a Phoneword",
+            };
+
+            var entry = new Entry
+            {
+                Placeholder = "Name",
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var buttonTranslate = new Button
+            {
+                Text = "Translate",
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            var buttonCall = new Button
+            {
+                Text = "Call 1-885-9262746",
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            mainStackLayout.Children.Add(labelTitle);
+            mainStackLayout.Children.Add(entry);
+            mainStackLayout.Children.Add(buttonTranslate);
+            mainStackLayout.Children.Add(buttonCall);
+
+            buttonCall.Clicked += (sender, e) =>
+            {
+                entry.Text = buttonCall.Text;
+            };
+
+            buttonTranslate.Clicked += (sedner, e) =>
+            {
+                entry.Text = null;
+            };
+
+
+            this.Content = mainStackLayout;
+        }
+
+    }
+}
