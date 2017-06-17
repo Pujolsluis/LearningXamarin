a70366d66f69776b0fc3f7d2dd054127ff5d292f
Luis Pujols <luispujols@hotmail.com>
2017-06-16 23:23:10 -0400

    ADD 'Created the image scale logic, and now the assignment is completly functional and completed'

--- Week2/OnsiteProject1/OnsiteProject1/OnsiteProject1/ButtonPage.xaml.cs
+++ Week2/OnsiteProject1/OnsiteProject1/OnsiteProject1/ButtonPage.xaml.cs
@@ -16,5 +16,25 @@ namespace OnsiteProject1
         {
             InitializeComponent();
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Button tempButton = (Button)sender;
+            switch (tempButton.Text)
+            {
+                case "Small":
+                    platformLogo.Scale = 1;
+                    break;
+                case "Medium":
+                    platformLogo.Scale = 1.5;
+                    break;
+                case "Large":
+                    platformLogo.Scale = 2.0;
+                    break;
+                default:
+                    break;
+            }
+        }
+
     }
 }
