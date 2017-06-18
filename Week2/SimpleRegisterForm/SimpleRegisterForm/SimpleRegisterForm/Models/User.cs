c124f03332c352ceeef5f79175c1e5d2d5232c2c
Luis Pujols <luispujols@hotmail.com>
2017-06-18 13:52:04 -0400

    ADD 'Created the base RegistrationViewModel'

--- Week2/SimpleRegisterForm/SimpleRegisterForm/SimpleRegisterForm/Models/User.cs
+++ Week2/SimpleRegisterForm/SimpleRegisterForm/SimpleRegisterForm/Models/User.cs
@@ -12,6 +12,8 @@ namespace SimpleRegisterForm.Models
         public string LastName { get; set; }
         public string Email { get; set; }
         public string Description { get; set; }
+        public string Password { get; set; }
         public bool AcceptedTerms { get; set; }
+
     }
 }
---
+++ Week2/SimpleRegisterForm/SimpleRegisterForm/SimpleRegisterForm/ViewModels/RegistrarionViewModel.cs
@@ -0,0 +1,31 @@
+﻿using SimpleRegisterForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleRegisterForm.ViewModels
+{
+    class RegistrarionViewModel
+    {
+        public User newUser { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string password { get; set; }
+        public string confirmPassword { get; set; }
+        public string description { get; set; }
+        public bool acceptedTerms { get; set; }
+
+        public RegistrarionViewModel()
+        {
+            newUser = new User();
+            firstName = newUser.FirstName;
+            lastName = newUser.LastName;
+            password = newUser.Password;
+            confirmPassword = newUser.Password;
+            description = newUser.Description;
+            acceptedTerms = newUser.AcceptedTerms;
+        }
+    }
+}
