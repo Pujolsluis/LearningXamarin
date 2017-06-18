5711dbed81a94668aebde7501e886f74513140af
Luis Pujols <luispujols@hotmail.com>
2017-06-18 18:27:26 -0400

    ADD 'Created the command for the signup button, and implemented the Fody Property Changed weaver, now the UI Elements update when their data updates in the viewmodel, the Project is fully functional and completed.'

--- Week2/SimpleRegisterForm/SimpleRegisterForm/SimpleRegisterForm/ViewModels/RegistrarionViewModel.cs
+++ Week2/SimpleRegisterForm/SimpleRegisterForm/SimpleRegisterForm/ViewModels/RegistrarionViewModel.cs
@@ -1,13 +1,15 @@
 ﻿using SimpleRegisterForm.Models;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace SimpleRegisterForm.ViewModels
 {
-    class RegistrarionViewModel
+    class RegistrarionViewModel : INotifyPropertyChanged
     {
         public User newUser { get; set; }
         public string firstName { get; set; }
@@ -17,17 +19,38 @@ namespace SimpleRegisterForm.ViewModels
         public string confirmPassword { get; set; }
         public string description { get; set; }
         public bool acceptedTerms { get; set; }
+        public Command signUp { get; set; }
 
         public RegistrarionViewModel()
         {
             newUser = new User();
-            firstName = newUser.FirstName;
-            lastName = newUser.LastName;
+            firstName = "Klk";
+            lastName = "Perez";
             password = newUser.Password;
             email = newUser.Email;
             confirmPassword = newUser.Password;
             description = newUser.Description;
             acceptedTerms = newUser.AcceptedTerms;
+            signUp = new Command(signUpCommand);
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void signUpCommand()
+        {
+            if((String.IsNullOrEmpty(firstName) || String.IsNullOrEmpty(lastName) || String.IsNullOrEmpty(email) ||
+                String.IsNullOrEmpty(password) || String.IsNullOrEmpty(confirmPassword) || String.IsNullOrEmpty(description)))
+            {
+                App.Current.MainPage.DisplayAlert("Error", "Fields cannot be empty", "OK");
+                password = null;
+                confirmPassword = null;
+            }
+            else
+            {
+                App.Current.MainPage.DisplayAlert("Welcome", "Thank you for signing up!", "OK");
+            }
         }
     }
+
 }
