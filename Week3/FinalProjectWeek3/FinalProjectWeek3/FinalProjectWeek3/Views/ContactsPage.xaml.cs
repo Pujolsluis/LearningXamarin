1a41cdb282ec25391a319f6223ee844f479c89a8
Luis Pujols <luispujols@hotmail.com>
2017-06-19 22:45:49 -0400

    ADD 'Created the Delete ContextAction for the ListView and now the app confirms and deletes a contact correctly'

--- Week3/FinalProjectWeek3/FinalProjectWeek3/FinalProjectWeek3/Views/ContactsPage.xaml.cs
+++ Week3/FinalProjectWeek3/FinalProjectWeek3/FinalProjectWeek3/Views/ContactsPage.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FinalProjectWeek3.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,6 +19,16 @@ namespace FinalProjectWeek3.Views
             InitializeComponent();
             this.Title = "My Contacts";
             BindingContext = new ViewModels.ContactsPageViewModel();
+        }
+
+        private async Task onDeleteAction(object sender, EventArgs e)
+        {
+            var myViewModel = (ViewModels.ContactsPageViewModel)this.BindingContext;
+            var menuItem = (MenuItem)sender;
+            var param = (Contact)menuItem.CommandParameter;
+            var answer = await App.Current.MainPage.DisplayAlert("Delete?", "Would you like to delete the contact: " + param.Name, "Yes", "No");
+            if(answer)
+                myViewModel.Contacts.Remove(param);
         }
     }
 }
