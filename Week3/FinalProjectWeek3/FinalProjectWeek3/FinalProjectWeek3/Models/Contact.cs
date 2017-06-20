70b99b94e1df9205a1cfddae8ec2cc2eefdf2447
Luis Pujols <luispujols@hotmail.com>
2017-06-19 20:14:14 -0400

    ADD 'Completed the ContactsPage View, and basic Binding with the ViewModel'

--- Week3/FinalProjectWeek3/FinalProjectWeek3/FinalProjectWeek3/Models/Contact.cs
+++ Week3/FinalProjectWeek3/FinalProjectWeek3/FinalProjectWeek3/Models/Contact.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FinalProjectWeek3.Models
 {
-    public class Contact
+    public class Contact : INotifyPropertyChanged
     {
         public string Name { get; set; }
         public string PhoneNumber { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
--- Week3/FinalProjectWeek3/FinalProjectWeek3/FinalProjectWeek3/ViewModels/ContactsPageViewModel.cs
+++ Week3/FinalProjectWeek3/FinalProjectWeek3/FinalProjectWeek3/ViewModels/ContactsPageViewModel.cs
@@ -11,13 +11,12 @@ namespace FinalProjectWeek3.ViewModels
 {
     class ContactsPageViewModel : INotifyPropertyChanged
     {
-        public ObservableCollection<Contact> Contacts { get; set; }
+        public ObservableCollection<Contact> Contacts { get; set; } = new ObservableCollection<Contact>();
 
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ContactsPageViewModel()
         {
-            Contacts = new ObservableCollection<Contact>();
             Contacts.Add(new Contact { Name = "Luis Pujols", PhoneNumber = "829-421-2414" });
             Contacts.Add(new Contact { Name = "Jennifer Pujols", PhoneNumber = "829-421-2413" });
         }
