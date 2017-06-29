df7837b7aae62ec2b0ae44a7c0a027669df658e7
Luis Pujols <luispujols@hotmail.com>
2017-06-29 18:59:41 -0400

    ADD 'Created the RoundedBoxViewRenderer Android Implementation'

--- Week4/CustomBoxViewRenderer/CustomBoxViewRenderer/CustomBoxViewRenderer.Android/RoundedBoxViewRenderer.cs
+++ Week4/CustomBoxViewRenderer/CustomBoxViewRenderer/CustomBoxViewRenderer.Android/RoundedBoxViewRenderer.cs
@@ -10,10 +10,46 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Xamarin.Forms.Platform.Android;
+using CustomBoxViewRenderer.Droid;
+using CustomBoxViewRenderer.Controls;
+using Xamarin.Forms;
+using Android.Graphics;
 
+[assembly: ExportRendererAttribute(typeof(RoundedBoxView), typeof(RoundedBoxViewRenderer))]
 namespace CustomBoxViewRenderer.Droid
 {
     class RoundedBoxViewRenderer : BoxRenderer
     {
+        public RoundedBoxViewRenderer()
+        {
+            this.SetWillNotDraw(false);
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            RoundedBoxView rbv = (RoundedBoxView)this.Element;
+
+            Rect rc = new Rect();
+            GetDrawingRect(rc);
+
+            Rect interior = rc;
+            interior.Inset((int)rbv.StrokeThickness, (int)rbv.StrokeThickness);
+
+            Paint p = new Paint()
+            {
+                Color = rbv.Color.ToAndroid(),
+                AntiAlias = true,
+            };
+
+            canvas.DrawRoundRect(new RectF(interior), (float)rbv.CornerRadius, (float)rbv.CornerRadius, p);
+
+            p.Color = rbv.Stroke.ToAndroid();
+            p.StrokeWidth = (float)rbv.StrokeThickness;
+            p.SetStyle(Paint.Style.Stroke);
+
+            canvas.DrawRoundRect(new RectF(interior), (float)rbv.CornerRadius, (float)rbv.CornerRadius, p);
+            
+        }
+
     }
 }
