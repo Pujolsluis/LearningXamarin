2dfe4e3f78c5b30603ca99b1922e50b9519b6a01
Luis Pujols <luispujols@hotmail.com>
2017-07-08 01:03:44 -0400

    ADD 'Installed the Libraries for JSON and Http Client'

--- Week5/FinalProjectWeek5/FinalProjectWeek5/FinalProjectWeek5/Views/WeatherPageView.xaml.cs
+++ Week5/FinalProjectWeek5/FinalProjectWeek5/FinalProjectWeek5/Views/WeatherPageView.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +18,25 @@ namespace FinalProjectWeek5.Views
         public WeatherPageView()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var data = getDataAsync().Result.ToString();
+            JObject jo = JObject.Parse(data);
+            foreach(JProperty property in jo["location"].Children())
+            {
+                if(property.Equals("name"))
+                    WeatherTitle.Text = property.Value.ToString();
+            }
+        }
+
+        public async Task<object> getDataAsync()
+        {
+            string Url = "https://api.apixu.com/v1/current.json?key=3cf69de69a9a4af78bc151306170107&q=Santo%20Domingo";
+            HttpClient client = new HttpClient();
+            string result = await client.GetStringAsync(Url);
+            return JsonConvert.DeserializeObject<object>(result);
         }
     }
 }
