b41f595931ae858bdc9fd98dd9168451791cef6b
Luis Pujols <luispujols@hotmail.com>
2017-07-07 21:02:30 -0400

    ADD 'Implemented the Network Connectivity check'

--- Week5/NetStatus/NetStatus/NetStatus/App.xaml.cs
+++ Week5/NetStatus/NetStatus/NetStatus/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Plugin.Connectivity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,15 @@ namespace NetStatus
         {
             InitializeComponent();
 
-            MainPage = new NetStatus.Views.NoNetworkPage();
+            if (CrossConnectivity.Current.IsConnected)
+            {
+                MainPage = new NetStatus.Views.NetworkViewPage();
+            }
+            else
+            {
+                MainPage = new NetStatus.Views.NoNetworkPage();
+            }
+            
         }
 
         protected override void OnStart()
