bc914813d7aafa36d2db458cccdd0eb4aad9fd05
Luis Pujols <luispujols@hotmail.com>
2018-03-17 13:04:54 -0400

    ADD "Implemented the Logic for the app, and now the app is completed"

---
+++ Week1/ColorPrNavRev/ColorPrNavRev/ColorPrNavRev/ColoredPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ColorPrNavRev
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class ColoredPage : ContentPage
+	{
+        private String userText;
+        private Color color;
+
+        public ColoredPage(String text, Color color)
+        {
+            InitializeComponent();
+
+            this.Title = "Result";
+            this.userText = text;
+            this.color = color;
+
+            baseLayoutContainer.BackgroundColor = color;
+            colorString.Text = userText;
+        }
+    }
+}
--- Week1/ColorPrNavRev/ColorPrNavRev/ColorPrNavRev/MainPage.xaml.cs
+++ Week1/ColorPrNavRev/ColorPrNavRev/ColorPrNavRev/MainPage.xaml.cs
@@ -15,7 +15,30 @@ namespace ColorPrNavRev
 		{
 			InitializeComponent();
 
-            
+            this.Title = "Color Processor Nav";
+            redButton.Clicked += onButtonClicked;
+		    blueButton.Clicked += onButtonClicked;
+		    greenButton.Clicked += onButtonClicked;
 		}
+
+	    private void onButtonClicked(object sender, EventArgs eventArgs)
+	    {
+	        Button tempButton = (Button) sender;
+
+            switch (tempButton.Text.ToLower())
+	        {
+                case "red":
+                    Navigation.PushAsync(new ColoredPage(userEntry.Text, Color.Red));
+                    break;
+                case "blue":
+                    Navigation.PushAsync(new ColoredPage(userEntry.Text, Color.Blue));
+                    break;
+                case "green":
+                    Navigation.PushAsync(new ColoredPage(userEntry.Text, Color.Green));
+                    break;
+                default:
+                    break;
+	        }
+	    }
 	}
 }
