0371f153247ea1f517a08495fad94115acd717ab
Luis Pujols <luispujols@hotmail.com>
2018-03-17 17:28:21 -0400

    ADD "Created and completed the Color Processor Text Editor Project"

---
+++ Week1/ColorPrEditRev/ColorPrEditRev/ColorPrEditRev/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ColorPrEditRev
+{
+	public partial class MainPage : ContentPage
+	{
+	    
+		public MainPage()
+		{
+			InitializeComponent();
+
+            sizePicker.Items.Add("Small");
+            sizePicker.Items.Add("Medium");
+            sizePicker.Items.Add("Large");
+
+            redButton.Clicked += buttonClicked;
+		    blueButton.Clicked += buttonClicked;
+		    greenButton.Clicked += buttonClicked;
+		}
+
+        FormattedString fs = new FormattedString();
+
+	    private void buttonClicked(object sender, EventArgs eventArgs)
+	    {
+	        Button tempButton = (Button) sender;
+
+	        switch (sizePicker.SelectedIndex)
+	        {
+                case 0:
+                    fs.Spans.Add( new Span { Text = textEntry.Text, ForegroundColor = tempButton.BackgroundColor, FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)) } );
+                    break;
+                case 1:
+                    fs.Spans.Add(new Span { Text = textEntry.Text, ForegroundColor = tempButton.BackgroundColor, FontSize = Device.GetNamedSize(NamedSize.Medium, typeof(Label)) });
+                    break;
+                case 2:
+                    fs.Spans.Add(new Span { Text = textEntry.Text, ForegroundColor = tempButton.BackgroundColor, FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)) });
+                    break;
+                default:
+                    fs.Spans.Add(new Span { Text = textEntry.Text, ForegroundColor = tempButton.BackgroundColor, FontSize = Device.GetNamedSize(NamedSize.Small, typeof(Label)) });
+                    break;
+	        }
+
+	        resultLabel.FormattedText = fs;
+
+	    }
+	}
+}
