41f0296175274b2c4cc0b5851a74e46ab5b8ffb4
Luis Pujols <luispujols@hotmail.com>
2018-03-18 01:47:37 -0400

    ADD "Created the base files to implement the MVVM Pattern in the project"

---
+++ Week2/SimpRegFormRev/SimpRegFormRev/SimpRegFormRev/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpRegFormRev.Models
+{
+    class User
+    {
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String ConfirmPassword { get; set; }
+        public bool acceptTerms { get; set; }
+
+    }
+}
