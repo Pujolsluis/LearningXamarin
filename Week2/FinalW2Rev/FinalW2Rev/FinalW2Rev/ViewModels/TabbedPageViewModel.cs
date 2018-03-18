51d7e674bd81874280dcf1ecbce0920b41707427
Luis Pujols <luispujols@hotmail.com>
2018-03-18 19:26:57 -0400

    ADD "Implemented the Fody Weaver and Notify Property to the project"

--- Week2/FinalW2Rev/FinalW2Rev/FinalW2Rev/ViewModels/TabbedPageViewModel.cs
+++ Week2/FinalW2Rev/FinalW2Rev/FinalW2Rev/ViewModels/TabbedPageViewModel.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Text;
+using FinalW2Rev.Droid.Annotations;
 
 namespace FinalW2Rev.ViewModels
 {
-    class TabbedPageViewModel
+    class TabbedPageViewModel : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
     }
 }
